97a4b09d166f1012b35ae8a972e6cfd1da2a54f9
Ashok <ashokkumarsingh@gmail.com>
2024-04-15 09:30:17 +0530

    15 April 2024: erpv2 NextAuth vioep Inspiration - Antonio's video offset 46:49

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,38 @@
-import {Button} from "@/components/ui/button";
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { LoginButton } from "@/components/auth/login-button";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"],
+});
 
 export default function Home() {
   return (
-  <>
-    <p className="text-green-500">Hello Auth!!!</p>;
-    <Button>Clicking Me</Button>
-  </>
+    <>
+      <main
+        className="flex h-full flex-col items-center justify-center
+         bg-slate-900 text-white"
+      >
+        <div className="space-y-6">
+          <h1
+            className={cn(
+              "text-6xl font-semibold text-white drop-shadow-md",
+              font.className
+            )}
+          >
+            Home Page
+          </h1>
+          <div>
+            <LoginButton>
+              <Button variant="secondary" size="lg">
+                Sign In
+              </Button>
+            </LoginButton>
+          </div>
+        </div>
+      </main>
+    </>
   );
 }
-
