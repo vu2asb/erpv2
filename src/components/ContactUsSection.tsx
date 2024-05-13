82ca61e751ffde3c3f362fe94b30b23add8bf3a5
Ashok <ashokkumarsingh@gmail.com>
2024-05-13 12:29:21 +0530

    erpv2 website 12:25 13-5-24 added the email field WebDevEducation NextJS forms... offset 14:20

--- src/components/ContactUsSection.tsx
+++ src/components/ContactUsSection.tsx
@@ -1,7 +1,36 @@
+"use client";
+
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+import { Form } from "@/components/ui/form";
+
 import { Label } from "@/components/ui/label";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+
+const formSchema = z.object({
+  emailAddress: z.string().email(),
+});
 
 const ContactUsSection = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      emailAddress: "",
+    },
+  });
+
+  const handleSubmit = () => {};
+
   return (
     <div className="flex flex-row w-full space-x-2 min-h-fit">
       <div className="border p-10 border-white w-[80%] bg-gradient-to-r from-violet-800 to-fuchsia-800">
@@ -12,23 +41,37 @@ const ContactUsSection = () => {
         <div className="ml-5 text-[15px]">
           <h3>Time: 9 AM to 6 PM</h3>
           <h3>Indian Standard Time (UTC+5:30) Monday through Saturday</h3>
-
-
         </div>
       </div>
 
-      <div className="border border-white w-[80%]">
-      <h3 className="mt-4">What's your preferance</h3> 
-      <RadioGroup defaultValue="option-one" className="mt-4">
-            <div className="flex items-center space-x-2">
-              <RadioGroupItem value="option-one" id="option-one" />
-              <Label htmlFor="option-one">Video Conferrance</Label>
-            </div>
-            <div className="flex items-center space-x-2">
-              <RadioGroupItem value="option-two" id="option-two" />
-              <Label htmlFor="option-two">WhatsApp voice call</Label>
-            </div>
-          </RadioGroup>
+      <div className="border border-white w-[80%] p-10">
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(handleSubmit)}
+            className="max-w-md w-full flex flex-col gap-4"
+          >
+            <FormField
+              control={form.control}
+              name="emailAddress"
+              render={(field) => {
+                return (
+                  <FormItem>
+                    <FormLabel>Email Address</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Email address"
+                        type="email"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                );
+              }}
+            />
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
       </div>
     </div>
   );
