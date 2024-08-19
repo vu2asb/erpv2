c046b0ca908283c665e816c1d5ee4f835e108e2d
Ashok <ashokkumarsingh@gmail.com>
2024-08-19 14:10:04 +0530

    erpv2 website 19.8.24 1:06 PM; same as previous version but with Zod validation integrated on JSON payload API end.

--- src/app/api/api-poc-json/route.ts
+++ src/app/api/api-poc-json/route.ts
@@ -1,8 +1,62 @@
 import { pool } from "@/lib/db";
 import dbConnect from "@/lib/dbconnect";
 import { NextRequest, NextResponse } from "next/server";
+import { z, ZodError } from "zod";
+import { log } from "console";
 
 dbConnect();
+
+// Creating our validation schema
+const userSchema = z.object({
+  first_name: z
+    .string({ required_error: "First Name is required" })
+    .trim()
+    .min(2, { message: "First Name should be atleast 2 charecters" })
+    .max(30, { message: "First Name should not be longer than 30 charecters" }),
+  last_name: z
+    .string({ required_error: "Last Name is required" })
+    .trim()
+    .min(2, { message: "Last Name should be atleast 2 charecters" })
+    .max(30, { message: "Last Name should not be longer than 30 charecters" }),
+  appl_email: z
+    .string({ required_error: "Email is required" })
+    .trim()
+    .email("Invalid email format")
+    .min(4, { message: "Email should be atleast 4 charecters" })
+    .max(30, { message: "Email should not be longer than 30 charecters" }),
+  reg_time: z
+    .string({
+      required_error: "Registration timestamp with time zone is required",
+    })
+    .trim()
+    .min(20, {
+      message: "Registration timestamp should be atleast 2 charecters",
+    })
+    .max(30, {
+      message: "Registration timestamp should not be longer than 30 charecters",
+    }),
+  webinar_ref: z
+    .string({ required_error: "Webinar reference is required" })
+    .trim()
+    .min(1, { message: "Webinar reference should be atleast 1 charecter" })
+    .max(20, {
+      message: "Webinar reference should not be longer than 20 charecters",
+    }),
+  webinar_note: z
+    .string({ required_error: "Webinar note text is required" })
+    .trim()
+    .min(1, { message: "Webinar note text should be atleast 1 charecter" })
+    .max(200, {
+      message: "Webinar note text should not be longer than 200 charecters",
+    }),
+});
+
+export async function GET() {
+  return new Response("GET Success", {
+    status: 200,
+  });
+}
+
 // Handling POST request
 export async function POST(req: NextRequest, res: NextResponse) {
   // Get the json data recieved with the POST request
@@ -26,36 +80,52 @@ export async function POST(req: NextRequest, res: NextResponse) {
       ""
   );
 
-  console.log("POST data sent as json:: " + stringifyData + "");
+  console.log("JSON payload sent from form to API:: " + stringifyData + "");
   // add code here to send and save data to database
 
   try {
-    // "INSERT INTO contact_us (name, email, client_date_time_tz, message) VALUES ('"+obj.tname+"', '"+obj.temail+"', '"+obj.tstamp+"', '"+obj.tmessage+"')";
-    const queryText =
-      "INSERT INTO webinar_reg (first, last, email, reg_time, webinar_ref, webinar_note) VALUES ($1, $2, $3, $4, $5, $6)";
+    const applicant = userSchema.parse({
+        first_name: obj.fName,
+        last_name: obj.lName,
+        appl_email: obj.email,
+        reg_time: obj.regTime,
+        webinar_ref: obj.webinarRef,
+        webinar_note: obj.webinarNote,
+      });
+      console.log("Looks like no Zod parse error");
+    try {
+      // "INSERT INTO contact_us (name, email, client_date_time_tz, message) VALUES ('"+obj.tname+"', '"+obj.temail+"', '"+obj.tstamp+"', '"+obj.tmessage+"')";
+      const queryText =
+        "INSERT INTO webinar_reg (first, last, email, reg_time, webinar_ref, webinar_note) VALUES ($1, $2, $3, $4, $5, $6)";
 
-    const result = await pool.query(queryText, [
-      obj.fName,
-      obj.lName,
-      obj.email,
-      obj.regTime,
-      obj.webinarRef,
-      obj.webinarNote,
-    ]);
+      const result = await pool.query(queryText, [
+        applicant.first_name,
+        applicant.last_name,
+        applicant.appl_email,
+        applicant.reg_time,
+        applicant.webinar_ref,
+        applicant.webinar_note
+      ]);
 
-    // const result = await pool.query(query);
-    const re = result.rows[0];
-    const t32 = JSON.stringify(result.rowCount) + ": Row(s) Added";
-    console.log("Row(s) added::" + JSON.stringify(result.rowCount) + "...");
+      // const result = await pool.query(query);
+      const re = result.rows[0];
+      const t32 = JSON.stringify(result.rowCount) + ": Row(s) Added";
+      console.log("Row(s) added::" + JSON.stringify(result.rowCount) + "...");
 
-    return new Response(JSON.stringify(t32), {
-      status: 200,
-    });
-  } catch {
-    return new Response("DB error while trying to insert record", {
-      status: 500,
-    });
+      return new Response(JSON.stringify(t32), {
+        status: 200,
+      });
+    } catch {
+      return new Response("DB error while trying to insert record", {
+        status: 500,
+      });
+    }
+  } catch(error) {
+    if (error instanceof z.ZodError) {
+      console.log("ZOD Error");
+      return new Response("ZOD Error", {
+        status: 500,
+      });
+    }
   }
-
-  // return new NextResponse("Dummy return value");
 }
