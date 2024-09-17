9c8097cd0c64649fd4dda92a1fb86abd8cfeac9e
Ashok <ashokkumarsingh@gmail.com>
2024-09-17 13:53:00 +0530

    erpv2 website 17-9-24 1:49PM prev+ fully responsive waiting room.

--- src/app/landings/lp-2/page.tsx
+++ src/app/landings/lp-2/page.tsx
@@ -4,6 +4,7 @@ import { Button } from "@/components/ui/button";
 import TimerLP from "@/components/timer-lp";
 import { CiCalendar } from "react-icons/ci";
 import { useRouter } from "next/router";
+import { useState, useEffect } from "react"; // we will need this to mantain the loading state
 
 // NOTE:
 // Set the webinar reference and webinar note that
@@ -17,7 +18,7 @@ const webinar_note = "Note for Web Ref-101";
 // Set the duration of the counter here in minutes e.g. 10
 // E.g.
 // const freqMins = 10; // Minuites
-const freqMins = 2;
+const freqMins = 20;
 
 const monthNames = [
   "January",
@@ -44,13 +45,17 @@ const LandingPage2 = () => {
   const nyear = now.getFullYear();
   const nmonth = now.getMonth() + 1;
   const nday = now.getDate();
-  const nhour = now.getHours();
+  let nhour = now.getHours();
   const nmin = now.getMinutes();
   const nseconds = now.getSeconds();
   // Show Now Time
   console.log("Present Year:", nyear);
   console.log("Present Month:", nmonth);
   console.log("Present Day:", nday);
+  nhour >= 12?(nhour-12): nhour;
+  // if (nhour >= 12) {
+  //   nhour -= 12;
+  // }
   console.log("Present Hour:", nhour);
   console.log("Present Min:", nmin);
   console.log("Present Sec:", nseconds);
@@ -72,6 +77,8 @@ const LandingPage2 = () => {
   } else {
     ampm = "AM";
   }
+
+  // const difference = freqMins;
 
   const monthIndex = tMonth;
   const monthName = monthNames[monthIndex - 1];
@@ -104,14 +111,70 @@ const LandingPage2 = () => {
   // const ttime = mynow.toISOString();
   // console.log("LP Target Time ISO String:: " + mynow);
 
+  const dYear = tYear - nyear;
+  const dMonth = tMonth - nmonth;
+  const dDay = tDay - nday;
 
-const dYear = tYear - nyear;
-const dMonth = tMonth - nmonth;
-const dDay = tDay - nday;
-const dHour = tHour - nhour;
-const dMin = tMin - nmin;
-const dSec = tSec - nseconds;
-console.log("Delta YY: "+dYear+", MNT"+dMonth+", DD: "+dDay+", HH: "+dHour+", Min: "+dMin+", SS: "+dSec+"");
+// if(nhour >= 12){
+//   nhour = 0;
+// }
+ 
+const dHour = tHour;
+
+
+
+  const dMin = tMin - nmin;
+  const dSec = tSec - nseconds;
+  console.log(
+    "Difference YY: " +
+      dYear +
+      ", MNT " +
+      dMonth +
+      ", DD: " +
+      dDay +
+      ", HH: " +
+      dHour +
+      ", Min: " +
+      dMin +
+      ", SS: " +
+      dSec +
+      ""
+  );
+
+  let [timeLeft, settimeLeft] = useState(freqMins * 60); // Timer set for webinar repeat
+
+  let goLiveMinute = 0;
+  let goLiveSecond = 0;
+
+  useEffect(() => {
+    const intervalId = setInterval(function () {
+      timeLeft--;
+      //   console.log("useEffect for Timer function triggered; Count = " + count + "");
+      if (timeLeft < 0) {
+        clearInterval(intervalId);
+        console.log("Go-Live");
+      } else {
+        // Calculating time to go Live
+        goLiveMinute = Math.floor((timeLeft / 60) % 60);
+        goLiveSecond = Math.floor(timeLeft % 60);
+
+        console.log(
+          "Time set to Go-Live: " +
+            freqMins +
+            " Mins & " +
+            ((freqMins * 60) % 60) +
+            " Secs"
+        );
+        console.log(
+          "Time left to Go-Live: " +
+            goLiveMinute +
+            " Mins & " +
+            goLiveSecond +
+            " Secs"
+        );
+      }
+    }, 1000); // setInterval value in milli seconds; e.g. 1000 means 1000ms = 1 sec
+  });
 
   return (
     <main className="container mx-auto h-screen flex justify-center items-center">
@@ -137,9 +200,9 @@ console.log("Delta YY: "+dYear+", MNT"+dMonth+", DD: "+dDay+", HH: "+dHour+", Mi
                   "&targetHour=" +
                   dHour +
                   "&targetMin=" +
-                  dMin +
+                  goLiveMinute +
                   "&targetSec=" +
-                  dSec;
+                  goLiveSecond;
                 window.location.href = url;
               }}
               className="text-xl"
--- src/app/waiting-room/page.tsx
+++ src/app/waiting-room/page.tsx
@@ -1,11 +1,8 @@
 "use client";
 
 import Image from "next/image";
-import TimerLP from "@/components/timer-lp";
 import { useSearchParams } from "next/navigation";
 import React, { useState, useEffect } from "react";
-
-const doorToOpen = 10;
 
 const WaitingRoom = () => {
   // from registeration page:: const urlStringToPass = "/waiting-room?year="+tgtYear+"&month="+tgtMonth+"&day="+tgtDay+"&hour="+tgtHour+"&min="+tgtMin+"&sec="+tgtSec+"";
@@ -20,7 +17,12 @@ const WaitingRoom = () => {
   const tgtSec = searchParams.get("sec");
 
   const countDown =
-    (Number(tgtYear)*60*60*24*30*12)+(Number(tgtMonth)*60*60*24*30)+(Number(tgtDay)*60*60*24)+(Number(tgtHour) * 60 * 60) + (Number(tgtMin) * 60) + (Number(tgtSec));
+    Number(tgtYear) * 60 * 60 * 24 * 30 * 12 +
+    Number(tgtMonth) * 60 * 60 * 24 * 30 +
+    Number(tgtDay) * 60 * 60 * 24 +
+    Number(tgtHour) * 60 * 60 +
+    Number(tgtMin) * 60 +
+    Number(tgtSec);
   console.log("Waiting Room Calculated seconds: " + countDown);
 
   console.log(
@@ -39,58 +41,21 @@ const WaitingRoom = () => {
       ""
   );
 
-  //-----------------experiment begins------------------
-  // function createISO8601FromVariables(
-  //   year: number,
-  //   month: number,
-  //   day: number,
-  //   hour: number,
-  //   minute: number,
-  //   second: number
-  // ): string {
-  //   // Note: JavaScript months are 0-indexed (January is 0, December is 11), so we subtract 1 from the month.
-  //   const date = new Date(year, month - 1, day, hour, minute, second);
-
-  //   // Convert the Date object to ISO 8601 format
-  //   const iso8601String = date.toISOString();
-
-  //   console.log("ISO 8601 String: ", iso8601String);
-  //   return iso8601String;
-  // }
-  // // Example usage
-  // const year = tgtYear; //2024;
-  // const month = tgtMonth; //9;    // September
-  // const day = tgtDay; //13;
-  // const hour = tgtHour; // 18;
-  // const minute = tgtMin; //49;
-  // const second = tgtSec; //16;
-
-  // const isoDate = createISO8601FromVariables(
-  //   Number(year),
-  //   Number(month),
-  //   Number(day),
-  //   Number(hour),
-  //   Number(minute),
-  //   Number(second)
-  // );
-  // const mynow = new Date();
-  // mynow.setMinutes(mynow.getMinutes() + doorToOpen); // The counter loading time is set here
-  // const isoDateString = mynow.toISOString();
-  // console.log("ISO 8601 string:", isoDateString);
-  //-----------------experiment ends------------------
-
+  // const totCountDown = tgtMin + tgtSec;
   //---------------Timer Begins-----------------------
   const totCount = Number(tgtMin) * 60; // count down in seconds
-  console.log("Waiting Room Countdown [sec]: " + totCount);
+  console.log("Waiting Room Countdown Set to [Min]: " + tgtMin);
+  console.log("Waiting Room Countdown Set to [sec]: " + tgtSec);
 
-  let [count, setCount] = useState(totCount); // Timer in seconds
+  let [count, setCount] = useState(Number(tgtMin) * 60 + Number(tgtSec)); // Timer in seconds
 
   useEffect(() => {
     const intervalId = setInterval(function () {
-      count-=0.5;
+      count -= 0.5;
       if (count == 0) {
         clearInterval(intervalId);
-        console.log("Timeout :: Interval cleared.");
+        // console.log("Timeout :: Interval cleared.");
+        console.log("Proceeding to webinar ...");
         // redirect to webinar player page
         window.location.href = "/player";
       } else {
@@ -102,9 +67,17 @@ const WaitingRoom = () => {
         if (countElement) {
           // TypeScript knows at this point countElement is not null
           // You can do something like this now -> countElement.innerHTML = `${count}`;
-          const minutes = Math.floor(count / 60); // Get the whole minutes
-          const seconds = count % 60; // Get the remaining seconds
-          countElement.innerHTML = "" + minutes + " Min " + seconds + " Sec";
+          let minutes = Math.floor(count / 60); // Get the whole minutes
+          let seconds = count % 60; // Get the remaining seconds
+          let timeDisplay = "";
+          if (minutes > 0.5) {
+            timeDisplay = minutes.toString() + " Min ";
+          }
+          if (seconds > 0.5) {
+            timeDisplay += seconds.toString() + " Sec ";
+          }
+
+          countElement.innerHTML = "" + timeDisplay + "";
         } else {
           console.error('Element with id "counter" not found');
         }
@@ -112,77 +85,30 @@ const WaitingRoom = () => {
     }, 1000); // setInterval value in milli seconds; e.g. 1000 means 1000ms = 1 sec
   });
 
-   //---------------Timer Ends-----------------------
+  //---------------Timer Ends-----------------------
   return (
-    <div style={{ position: "relative", width: "100%", height: "100%" }}>
-      <Image
-        src="/assets/waiting-area.jpg" // Replace with your image path
-        alt="Your Image"
-        layout="fill"
-        objectFit="cover"
-      />
-      <div
-        style={{
-          position: "absolute",
-          fontSize: "40px",
-          top: 40,
-          left: 0,
-          right: 0,
-          padding: "20px",
-          margin: "20px",
-          backgroundColor: "rgba(0, 0, 0, 0.7)",
-          color: "white",
-          textAlign: "center",
-        }}
-      >
-        Welcome to the Waiting Room
-      </div>
-      <div
-        style={{
-          position: "absolute",
-          fontSize: "30px",
-          top: 500,
-          left: 650,
-          right: 650,
-          padding: "10px",
-          margin: "20px",
-          backgroundColor: "rgba(0, 0, 0, 0.7)",
-          color: "white",
-          textAlign: "center",
-          // placeItems: "center"
-        }}
-      >
-        Webinar begins in
-      </div>
+    <>
+<div className="bg-cover w-full h-full bg-no-repeat bg-[url('/assets/buildings.jpg')]">
+  <div className="mx-auto flex items-center justify-center h-screen">
+    <div className="w-[82%] h-[30.2%] bg-slate-800 opacity-90 border rounded text-center sm:h-[65%] lg:h-[32vh] xl:h-[45vh] 2xl:h-[31vh] 2xl:w-[35%]">
 
-      <div
-        style={{
-          position: "absolute",
-          fontSize: "30px",
-          top: 650,
-          left: 650,
-          right: 650,
-          padding: "10px",
-          margin: "20px",
-          backgroundColor: "rgba(0, 0, 0, 0.7)",
-          color: "white",
-          textAlign: "center",
-          // placeItems: "center"
-        }}
-      >
-        <p id="counter">Count down info </p>
-      </div>
-      <div className="flex justify-center items-center mx-10">
-        {/* <TimerLP launchDate={isoDateString} /> */}
-        {/* 2024: Year
-            10: Month
-            15: Day
-            T: Separator between date and time
-            17: Hour (in 24-hour format)
-            00: Minute
-            00: Second */}
-      </div>
+
+      <h1 className="text-gray-50 text-[5vw] mt-[4vh] sm:text-green-500 md:text-yellow-400 lg:text-blue-500 xl:text-pink-500 2xl:text-lime-400 2xl:text-[1.5vw]">
+        Welcome to the waiting room
+      </h1>
+      <h1 className="text-gray-50 text-[3.8vw] mt-[3vh] lg:text-[3.2vw] 2xl:text-[1.4vw]">
+        Kindly settle down
+      </h1>
+      <h1 className="text-gray-50 text-[2.5vw] mt-[4vh] mb-[1.8vh] lg:text-[2vw] mt-[4vh] 2xl:text-[1vw]">
+        Webinar begins in ...
+      </h1>
+
+      <span className="text-[4vw] text-orange-700 sm:text-[3vw] lg:text-[1.4vw] 2xl:text-[1.5vw]" id="counter"></span>
     </div>
+  </div>
+</div>
+
+    </>
   );
 };
 
